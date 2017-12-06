85eb40c74581a27d5fa28cb6a391c2fb1cd21315
Paul Ma <kwn791122@gmail.com>
2017-12-06 18:09:12 +0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,15 +20,23 @@ app.post('/todos', (req, res) => {
     // 繼承todo的類
     // 因為是post的關係所以我們可以輸入參數
     // 例如在postman輸入之類的
-    const todo = new Todo({
-        text: req.body.text
-    })
+    const todo = new Todo({ text: req.body.text });
     // 儲存這邊的變數todo
     todo.save().then(
         // 儲存成功就send我們所輸入的text回來
         doc => res.status(200).send(doc), 
         e => res.status(400).send(e)
     );
+});
+
+// GET 查詢route
+app.get('/todos', (req, res) => {
+    Todo.find().then(todos => {
+        // 這邊使用send method回傳從DB找來的Todo資料
+        // 之所以要用object是因為之後如果我們要定義像是status之類的
+        // 比較方便
+        res.send({ todos });
+    }, e => res.status(400).send(e));
 });
 
 app.listen(3000, () => console.log('Started on port 3000') );
