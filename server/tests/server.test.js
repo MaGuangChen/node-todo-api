c03f96a0a7c27c851fde236278c43afde8953e15
Paul Ma <kwn791122@gmail.com>
2017-12-06 18:28:16 +0800

    Add GET /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,11 @@ const request = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+const todos = [
+    { text: 'First test todo' },
+    { text: 'Second test todo' }
+];
+
 // testing lifecycle method
 // beforeEach可以在我們跑每個test case前先跑一段code
 // 我們這邊要使用是因為在我們的database表單內已經有一些資料了
@@ -13,7 +18,10 @@ beforeEach((done) => {
     // .remove() method會把我們的資料庫清空
     // 要求一個參數object，也可以在object帶參數去remove符合屬性的data
     // 接著chain .then 呼叫done告訴這項async操作已經結束
-    Todo.remove({}).then(() => done());
+    Todo.remove({})
+    // .inserMany是可以插入多筆data到DB collection的方法
+    .then(() => Todo.insertMany(todos))
+    .then(() => done());
 });
 
 describe('Post /todos', () => {
@@ -32,15 +40,15 @@ describe('Post /todos', () => {
         // 這次帶入既有的error, response兩參數來進行如果發生錯誤的handle
         .end((err, res) => {
             // 這邊用return只是為了在發生錯誤時能夠直接讓code停在這
-            if(err) { return done(err) }
+            if(err) return done(err);
 
             // .find menthod可找在這張表單的所有資料
             // 在noSQL是找這collection的所有資料
             // todos是.find後所在Todo的所有todo data
             // 也就是我們之前假設的doc data，因為在.then內第一個
             // callback func 是成功function自帶的參數為response回來的東西
-            Todo.find().then((todos) => {
-                // 因為在這個假測試中我們只有一筆資料
+            Todo.find({ text }).then((todos) => {
+                // 因為在這個假測試中我們符合text的資料只有一筆
                 expect(todos.length).toBe(1);
                 // 期望假資料text跟回傳的資料一樣
                 expect(todos[0].text).toBe(text);
@@ -62,9 +70,20 @@ describe('Post /todos', () => {
             if(err) { return done(err) };
             
             Todo.find().then((resTodos) => {
-                expect(resTodos.length).toBe(0);
+                // 因為前面有插入兩筆資料所以要等於2
+                expect(resTodos.length).toBe(2);
                 done();
             }).catch(e => done(e));
         })
     });
 });
+
+describe('GET /todos', (done) => {
+    it('Should get all todos', () => {
+        request(app).get('/todos')
+        .expect(200).expect(res => {
+            expect(res.req.body.todos.length).toBe(2);
+            expect(res.req.body.todos).toEqual(todos)
+        }).end(done);
+    });
+})
