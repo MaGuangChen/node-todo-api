ec119947dc4b975e35169d1b3b5945f96d23c798
Paul Ma <kwn791122@gmail.com>
2017-12-06 19:27:10 +0800

    add query playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('../server/db/mongoose');
+const { Todo } = require('../server/models/todo');
+const { User } = require('../server/models/user');
+// const id = '5a27c5f2cecd2210055e5a3b';
+
+// ObjectID.isValid(id) 可幫忙驗證這個id是否有效
+// if(!ObjectID.isValid(id)) {
+//     console.log('ID not valid')
+// }
+
+// 雖說mongo裡面的_id屬性值的資料型別為object ID
+// 但我們傳字串進去mongo也會幫我們轉為object ID
+// Todo.find({ _id: id }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// // 只回找符合輸入參數的一個，也就是第一個府和的
+// Todo.findOne({ _id: id }).then(todo => console.log('Todo', todo));
+
+// 利用id找todo
+// Todo.findById(id).then(todo => {
+//     if(!todo) return console.log('Id not found');
+//     console.log('find todo by id', todo)
+// }).catch(e => console.log(e));
+
+const id = "5a264d9309950388134be482";
+const idValid = ObjectID.isValid(id);
+if(!idValid) console.log('ID not valid');
+
+User.findById({ _id: id })
+.then(
+    user => {
+        if(!user) return console.log('User not found');
+        console.log(`Find User by ID ${JSON.stringify(user, undefined, 2)}`)
+    },(err) => console.log(err)
+);
